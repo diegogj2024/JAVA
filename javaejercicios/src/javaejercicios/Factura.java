0273ba74ebaccb47ebf0645ff5d893c388adc339
SebastianToloza <diegogarcia0809@outlook.com>
2025-02-14 09:31:51 -0500

    .

--- javaejercicios/src/javaejercicios/Factura.java
+++ javaejercicios/src/javaejercicios/Factura.java
@@ -9,18 +9,44 @@ import java.util.Scanner;
 public class Factura {
     String  nombre;
     int precio;
+    int cantidad;
+    String rol;
     public Factura(){
        Scanner objTeclado= new Scanner(System.in);
        System.out.println("ingrese el nombre producto");
        nombre=objTeclado.nextLine();
        System.out.println("ingrese el precio del producto");
        precio=objTeclado.nextInt();
-       Factura.imprimir(nombre,precio);
+       System.out.println("ingrese la cantidad del producto");
+       cantidad=objTeclado.nextInt();
+       System.out.println("¿eres estudiante?");
+       rol=objTeclado.nextLine();
+       Factura.procesar(nombre,precio,cantidad,rol);
        
     }
     
-    public static void imprimir(String nombre,int precio){
+    public static void procesar(String nombre,int precio,int cantidad,String rol){
+        int subtotal=precio*cantidad;
+        if (rol=="si"){
+            int total=(int)(subtotal*0.05);
+            rol="estudiante";
+            Factura.imprimir(nombre,precio,cantidad,rol,subtotal,total);
+        }
+        else{
+            int total=(int)(subtotal*0.13);
+            rol="persona normal";
+            Factura.imprimir(nombre,precio,cantidad,rol,subtotal,total);
+        }
+         
+    }
+    
+    public static void imprimir(String nombre,int precio,int cantidad,String rol,int subtotal,int total){
         System.out.println("nombre producto:"+nombre);
         System.out.println("precio producto:"+precio);
+        System.out.println("cantidad producto:"+cantidad);
+        System.out.println("rol cliente:"+rol);
+        System.out.println("subtotal:"+subtotal);
+        System.out.println("total:"+total);
+
     }
 }
