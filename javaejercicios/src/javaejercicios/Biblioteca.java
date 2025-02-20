6f4a5bb16db4095e06969c26b8171d2855af27de
SebastianToloza <>
2025-02-20 09:06:46 -0500

    .

---
+++ javaejercicios/src/javaejercicios/Biblioteca.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaejercicios;
+
+import java.util.Scanner;
+
+public class Biblioteca {
+    static Scanner objScanner=new Scanner(System.in);
+    static int contador;
+    static String acumular=" ";
+    static String acumulador1=" ";
+    static String acumulador=" ";
+    
+    public Biblioteca(){
+        System.out.println("bienvenido a la biblioteca leeme esta");
+        Procesar();
+    }
+    
+    public static void Procesar(){
+        
+    }
+}
